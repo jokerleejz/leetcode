ae08b45fb161b38f66b5a2f1d03ba3859e91c033
847003436@qq.com <mlyd189154>
2020-09-04 23:05:08 +0800

    滑动窗口处理最小子字符串问题

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,5 +1,38 @@
+import java.util.*;
+
 public class LeetCode {
     public static void main(String[] args) {
-        System.out.println("Let's leetcode");
+        String s = "bbbbb";
+        System.out.println(lengthOfLongestSubstring(s));
     }
+
+    public static int lengthOfLongestSubstring(String s) {
+        int maxLength = 0;
+        Set<Character> set = new HashSet<Character>();
+        int k = 0;
+        for(int i = 0, j = s.length(); i < j; i ++){
+            int max = 0;
+            if(i > 0){
+                set.remove(s.charAt(i-1));
+            }
+            while(k<j) {
+                if(k < i){
+                    k = i;
+                }
+                if(set.contains(s.charAt(k))){
+                    break;
+                }
+                else{
+                    set.add(s.charAt(k));
+                    k ++;
+                }
+            }
+            max = k-i;
+            if(maxLength < max){
+                maxLength = max;
+            }
+        }
+        return maxLength;
+    }
+
 }
