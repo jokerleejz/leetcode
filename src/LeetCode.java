239a947b90e037ebdc12f452f8c67be3764f2268
847003436@qq.com <mlyd189154>
2020-09-06 21:35:14 +0800

    双指针处理

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,5 +1,27 @@
 public class LeetCode {
     public static void main(String[] args) {
-        System.out.println("Let's leetcode");
+        int[] list = new int[]{1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(list));
+    }
+
+    public static int maxArea(int[] height) {
+        int max = 0;
+        int length = height.length;
+        //双指针
+        int i = 0, j = length-1;
+        while(i != j){
+            int s = Math.min(height[i], height[j]) * (j-i);
+            if(max < s){
+                max = s;
+            }
+            //移动较小的数的指针
+            if(height[i] <= height[j]){
+                i ++;
+            }
+            else{
+                j --;
+            }
+        }
+        return max;
     }
 }
