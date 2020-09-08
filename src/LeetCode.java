3fb261d193dc68990af5d58e017e850f9b641fe8
847003436@qq.com <mlyd189154>
2020-09-08 20:51:35 +0800

    双指针法，与M#15类似

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,5 +1,34 @@
+import java.util.Arrays;
+
 public class LeetCode {
     public static void main(String[] args) {
         System.out.println("Let's leetcode");
+    }
+
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int length = nums.length;
+        int rt = 100000;
+        for(int first = 0; first < length; first ++) {
+            int third = length - 1;
+            for(int second = first + 1; second < length; second ++) {
+                while(second < third) {
+                    int sum = nums[first] + nums[second] + nums[third];
+                    if(sum == target) {
+                        rt = sum;
+                    }
+                    else if(Math.abs(sum-target) < Math.abs(rt-target)) {
+                        rt = sum;
+                    }
+                    if(sum > target) {
+                        third --;
+                    }
+                    else {
+                        second ++;
+                    }
+                }
+            }
+        }
+        return rt;
     }
 }
