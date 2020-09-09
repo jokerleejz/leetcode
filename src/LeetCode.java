bf7317c1e143f5ce70078a0604d3794283b99a1a
847003436@qq.com <mlyd189154>
2020-09-09 22:49:20 +0800

    双指针的变形，三指针？差不多就这个意思

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,5 +1,48 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class LeetCode {
     public static void main(String[] args) {
-        System.out.println("Let's leetcode");
+        int[] nums = new int[]{1, 0, -1, 0, -2, 2};
+        int target = 0;
+        fourSum(nums, target);
+    }
+
+    public static List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> lists = new ArrayList<>();
+        int length = nums.length;
+        int sum = 0;
+        for (int first = 0; first < length - 2; first++) {
+            //去重
+            if(first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+            for(int second = first + 1; second < length - 2; second++) {
+                //去重
+                if(second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+                //双指针
+                int third = second + 1,forth = nums.length - 1;
+                while (third < forth) {
+                    sum = nums[first] + nums[second] + nums[third] + nums[forth];
+                    if(target < sum) {
+                        //大了，右指针左移
+                        while (third < forth && nums[forth] == nums[--forth]);
+                    } else if(target > sum) {
+                        //小了，左指针右移
+                        while (third < forth && nums[third] == nums[++third]);
+                    } else {
+                        lists.add(new ArrayList<>(Arrays.asList(nums[first],nums[second],nums[third],nums[forth])));
+                        //得到答案后找到下一个不一样的元素
+                        while (third < forth && nums[third] == nums[++third]);
+                        while(third < forth && nums[forth] == nums[--forth]);
+                    }
+                }
+            }
+        }
+        return lists;
     }
 }
