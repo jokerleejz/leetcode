f0a0984db0253013168ebc79efc82ca8fed818bb
847003436@qq.com <mlyd189154>
2020-09-12 23:10:53 +0800

    找当前节点右边第一个比它小的节点

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,5 +1,36 @@
 public class LeetCode {
     public static void main(String[] args) {
-        System.out.println("Let's leetcode");
+        int[] nums = new int[]{1,2,3};
+        nextPermutation(nums);
+    }
+
+    public static void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1);
+    }
+
+    private static void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
