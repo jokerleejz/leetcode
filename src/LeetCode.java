493a72009fc6f90503d7f07dc3cf91ff0b0fbdff
847003436@qq.com <mlyd189154>
2020-09-15 21:42:30 +0800

    先排序后回溯

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,5 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class LeetCode {
     public static void main(String[] args) {
-        System.out.println("Let's leetcode");
+        int[] candidates = new int[]{2,3,5};
+        int target = 8;
+        combinationSum(candidates, target);
+    }
+
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> combine = new ArrayList<Integer>();
+        dfs(candidates, target, ans, combine, 0);
+        return ans;
+    }
+
+    public static void dfs(int[] candidates, int target, List<List<Integer>> ans, List<Integer> combine, int idx) {
+        if (idx == candidates.length) {
+            return;
+        }
+        if (target == 0) {
+            ans.add(new ArrayList<Integer>(combine));
+            return;
+        }
+        // 选择当前数
+        if (target - candidates[idx] >= 0) {
+            combine.add(candidates[idx]);
+            dfs(candidates, target - candidates[idx], ans, combine, idx);
+            combine.remove(combine.size() - 1);
+        }
+        else{
+            return;
+        }
+        // 选择下一个数
+        dfs(candidates, target, ans, combine, idx + 1);
     }
 }
