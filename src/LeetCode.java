2834eb6ef6f0028454f77c104f248b6b2f551a86
847003436@qq.com <mlyd189154>
2020-09-18 22:45:22 +0800

    最基本的回溯

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,5 +1,36 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
 public class LeetCode {
+    static Deque<Integer> path = new ArrayDeque<>();
+
     public static void main(String[] args) {
-        System.out.println("Let's leetcode");
+        int[] nums = new int[] {1,2,3};
+        permute(nums);
+    }
+
+    public static List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> rt = new ArrayList<List<Integer>>();
+        dfs(nums, rt);
+        return rt;
+    }
+
+    private static void dfs(int[] nums, List<List<Integer>> rt) {
+        if(path.size() == nums.length) {
+            rt.add(new ArrayList<>(path));
+            return;
+        }
+        for(int j = 0; j < nums.length; j ++) {
+            if(path.contains(nums[j])) {
+                continue;
+            }
+            path.addLast(nums[j]);
+            //System.out.println("before:"+path);
+            dfs(nums, rt);
+            path.removeLast();
+            //System.out.println("after:"+path);
+        }
     }
 }
