df8211e43487fb7f432a78c343720cfea627e594
847003436@qq.com <mlyd189154>
2020-09-19 20:08:04 +0800

    在M#46的基础上排序并且添加剪枝条件

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,5 +1,41 @@
+import java.util.*;
+
 public class LeetCode {
+    static Deque<Integer> path = new ArrayDeque<>();
+    static boolean[] used = null;
+
     public static void main(String[] args) {
-        System.out.println("Let's leetcode");
+        int[] nums = new int[] {3,3,0,3};
+        permuteUnique(nums);
+    }
+
+    public static List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> rt = new ArrayList<List<Integer>>();
+        used = new boolean[nums.length];
+        Arrays.sort(nums);
+        dfs(nums, rt);
+        return rt;
+    }
+
+    private static void dfs(int[] nums, List<List<Integer>> rt) {
+        if(path.size() == nums.length) {
+            rt.add(new ArrayList<>(path));
+            return;
+        }
+        for(int j = 0; j < nums.length; j ++) {
+            if(used[j]) {
+                continue;
+            }
+            if(j>0 && nums[j]==nums[j-1] && !used[j-1]) {
+                continue;
+            }
+            path.addLast(nums[j]);
+            used[j] = true;
+            System.out.println("before:"+path);
+            dfs(nums, rt);
+            path.removeLast();
+            used[j] = false;
+            System.out.println("after:"+path);
+        }
     }
 }
