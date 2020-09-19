103cd0f66cf850637847e79200adccc1b66d30c0
847003436@qq.com <mlyd189154>
2020-09-19 20:18:53 +0800

    转置加翻转

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,4 +2,24 @@ public class LeetCode {
     public static void main(String[] args) {
         System.out.println("Let's leetcode");
     }
+
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+        //转置
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int tmp = matrix[j][i];
+                matrix[j][i] = matrix[i][j];
+                matrix[i][j] = tmp;
+            }
+        }
+        //翻转
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = tmp;
+            }
+        }
+    }
 }
