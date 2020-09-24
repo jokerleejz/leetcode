af600c6ca6dc40cc4c613273ce95d959930ed53c
847003436@qq.com <mlyd189154>
2020-09-24 21:20:57 +0800

    蛮简单的

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,5 +1,25 @@
 public class LeetCode {
     public static void main(String[] args) {
-        System.out.println("Let's leetcode");
+        int[] list = new int[] {3,2,1,0};
+        System.out.println(canJump(list));
+    }
+
+    public static boolean canJump(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return false;
+        }
+        int len = nums.length;
+        if(nums.length == 1) {
+            return true;
+        }
+        int farest = 0;
+        for (int i = 0; i < len; i++)
+        {
+            if (i > farest) {
+                return false;
+            }
+            farest = Math.max(farest, i + nums[i]);
+        }
+        return true;
     }
 }
