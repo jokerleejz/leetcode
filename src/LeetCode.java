8ef1442f246c9e9ec5082250dd820a712760b08f
847003436@qq.com <mlyd189154>
2020-10-02 09:46:38 +0800

    原地算法，用时间作为代价

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,4 +2,36 @@ public class LeetCode {
     public static void main(String[] args) {
         System.out.println("Let's leetcode");
     }
+
+    public void setZeroes(int[][] matrix) {
+        int MODIFIED = -2000000;
+        int R = matrix.length;
+        int C = matrix[0].length;
+
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                if (matrix[r][c] == 0) {
+                    for (int k = 0; k < C; k++) {
+                        if (matrix[r][k] != 0) {
+                            matrix[r][k] = MODIFIED;
+                        }
+                    }
+                    for (int k = 0; k < R; k++) {
+                        if (matrix[k][c] != 0) {
+                            matrix[k][c] = MODIFIED;
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int r = 0; r < R; r++) {
+            for (int c = 0; c < C; c++) {
+                // Make a second pass and change all MODIFIED elements to 0 """
+                if (matrix[r][c] == MODIFIED) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+    }
 }
