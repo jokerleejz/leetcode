d26174436d6f2b76243e2617c61dd1c3ce4b3d23
847003436@qq.com <mlyd189154>
2020-10-04 16:17:54 +0800

    回溯

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,5 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeetCode {
     public static void main(String[] args) {
-        System.out.println("Let's leetcode");
+        int[] nums = new int[]{1,2,3};
+        System.out.println(subsets(nums));
+    }
+
+    static List<Integer> t = new ArrayList<Integer>();
+    static List<List<Integer>> ans = new ArrayList<List<Integer>>();
+
+    public static List<List<Integer>> subsets(int[] nums) {
+        dfs(0, nums);
+        return ans;
+    }
+
+    public static void dfs(int cur, int[] nums) {
+        if (cur == nums.length) {
+            ans.add(new ArrayList<Integer>(t));
+            return;
+        }
+        t.add(nums[cur]);
+        dfs(cur + 1, nums);
+        t.remove(t.size() - 1);
+        dfs(cur + 1, nums);
     }
 }
