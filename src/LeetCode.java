5f31ad7bc0bc1b889bed1efe9927df450f8d7849
847003436@qq.com <mlyd189154>
2020-10-06 16:20:44 +0800

    双指针

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,5 +1,21 @@
 public class LeetCode {
     public static void main(String[] args) {
-        System.out.println("Let's leetcode");
+        int[] nums = new int[]{1,1,1,2,2,3};
+        System.out.println(removeDuplicates(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int j = 1, count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                count++;
+            } else {
+                count = 1;
+            }
+            if (count <= 2) {
+                nums[j++] = nums[i];
+            }
+        }
+        return j;
     }
 }
