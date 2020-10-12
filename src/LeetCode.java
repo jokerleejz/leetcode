34f410a502e42cec23fc617cac0088cff2412478
847003436@qq.com <mlyd189154>
2020-10-12 22:02:32 +0800

    没看懂题面

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2,4 +2,13 @@ public class LeetCode {
     public static void main(String[] args) {
         System.out.println("Let's leetcode");
     }
+
+    List<Integer> ans = new ArrayList<>();
+
+    public List<Integer> grayCode(int n) {
+        for(int i=0;i<(1<<n);i++){
+            ans.add(i^(i>>1));
+        }
+        return ans;
+    }
 }
