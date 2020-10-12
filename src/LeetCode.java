96af3c4e75d890a95e397572924a7799f0b96a23
847003436@qq.com <mlyd189154>
2020-10-12 21:58:14 +0800

    递归删除重复的首项

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1,5 +1,25 @@
 public class LeetCode {
     public static void main(String[] args) {
-        System.out.println("Let's leetcode");
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(2);
+        head.next.next.next.next = new ListNode(3);
+        deleteDuplicates(head);
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        if (head.val == head.next.val) {
+            while (head != null && head.next != null && head.val == head.next.val) {
+                head = head.next;
+            }
+            return deleteDuplicates(head.next);
+        } else {
+            head.next = deleteDuplicates(head.next);
+            return head;
+        }
     }
 }
